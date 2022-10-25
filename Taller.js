caa5fa1d98a6db45eebbfc815cb0f23a09512637
Freddygrego <100003363+Freddygrego@users.noreply.github.com>
2022-10-24 21:23:09 -0500

    Update Taller.js

--- Taller.js
+++ Taller.js
@@ -1,7 +1,6 @@
 function Cuadro(tama,letra="X"){
 
     if (typeof tama === 'number'){
-        tama = Math.pow(`${tama}`,1);
         let i = 0;
             while ( i < tama ){
              console.log(`${letra}`.repeat(tama)) 
